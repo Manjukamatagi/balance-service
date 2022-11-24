5a8ddd5dd916c2c99be8891403563f1e29c5dee2
Manjukamatagi <manjunathka@maveric-systems.com>
2022-11-25 00:55:03 +0530

    Added log statements

--- src/main/java/com/maveric/balanceservice/controller/BalanceController.java
+++ src/main/java/com/maveric/balanceservice/controller/BalanceController.java
@@ -2,6 +2,7 @@ package com.maveric.balanceservice.controller;
 
 import com.maveric.balanceservice.dto.BalanceDto;
 import com.maveric.balanceservice.service.BalanceService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +18,13 @@ public class BalanceController {
 
     @Autowired
     BalanceService balanceService;
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(BalanceController.class);
 
     //Returns the User Balance details By balance ID
 
     @GetMapping("accounts/{accountId}/balances/{balanceId}")
     public ResponseEntity<String> getBalanceDetails(@PathVariable String accountId,@PathVariable String balanceId) {
-//        log.info("API call returning balance for the given valid Account Id");
+        log.info("API call returning balance for the given valid Account Id");
         BalanceDto balanceDtoResponse = balanceService.getBalanceDetails(accountId,balanceId);
         return new ResponseEntity<>(String.valueOf(balanceDtoResponse.getAmount()), HttpStatus.OK);
     }
